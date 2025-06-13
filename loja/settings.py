155f80311f7aa4bfd4f46a0306f3472a94122417
Gustavo-Félix <gustavofelix2007@gmail.com>
2025-06-13 09:46:00 -0300

    Criando os models Produto e Variação

--- loja/settings.py
+++ loja/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pedido',
+    'perfil',
+    'produto',
 
     # TODO: Remover debug toolbar
     "debug_toolbar",
--- produto/admin.py
+++ produto/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Produto, Variacao
 
-# Register your models here.
+class VariacaoInline(admin.TabularInline):
+    model = Variacao
+    extra = 1
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = 'nome', 'descricao_curta', 'preco_marketing',
+    list_display_links = 'nome',
+    search_fields = 'id', 'nome', 
+    list_per_page = 10
+    ordering = '-id',
+    inlines = [VariacaoInline]
+
+admin.site.register(Variacao)
