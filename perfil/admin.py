3a2f8cbe73557dfcdc1bbafe5e64f681a0a73658
Gustavo-Félix <gustavofelix2007@gmail.com>
2025-06-16 08:32:24 -0300

    Criando o model do perfil de usuários

--- perfil/admin.py
+++ perfil/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Perfil
 
-# Register your models here.
+@admin.register(Perfil)
+class PerfilAdmin(admin.ModelAdmin):
+    list_display = 'usuario', 'idade', 'cidade',
+    list_display_links = 'usuario',
+    search_fields = 'id', 'usuario', 
+    list_per_page = 10
+    ordering = '-id',
