505b08d4a87343ac3e0b9d1ce05465c7ec3108fb
Gustavo-Félix <gustavofelix2007@gmail.com>
2025-06-30 09:39:34 -0300

    Página de detalhes sobre o produto

--- produto/views.py
+++ produto/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
 from django.http import HttpResponse
-from .models import Produto
+from .models import Produto, Variacao
 
 class ListaProdutos(ListView):
     model = Produto
@@ -10,9 +11,11 @@ class ListaProdutos(ListView):
     context_object_name = "produtos"
     paginate_by = 10
 
-class DetalheProduto(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('Detalhe')
+class DetalheProduto(DetailView):
+    model = Produto
+    template_name = "produto/detalhe.html"
+    context_object_name = "produto"
+    slug_url_kwarg = 'slug' 
 
 class AdicionarAoCarrinho(View):
     def get(self, *args, **kwargs):
